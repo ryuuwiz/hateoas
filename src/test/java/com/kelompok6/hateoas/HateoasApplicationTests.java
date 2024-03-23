f8f1c6f87ce8364e48bea245a8ed9aaed325f849
ryuuwiz <ryuuwiz@gmail.com>
2024-03-23 10:12:55 +0700

    initial setup

---
+++ src/test/java/com/kelompok6/hateoas/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok6.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
